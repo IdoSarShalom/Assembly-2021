619fcb18a6e32ee8dae936963dc760df085f7c2b
Ido Sar Shalom <79599027+IdoSarShalom@users.noreply.github.com>
2021-09-22 12:17:54 +0300

    Update Part2_Question4_A.asm

--- Exercise 2/Solutions/Part2_Question4_A.asm
+++ Exercise 2/Solutions/Part2_Question4_A.asm
@@ -1,7 +1,7 @@
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ; Part2_Question4_A.asm
 ; due to 14/04/2021
-; Ido Sar Shalom  212410146
+; Ido Sar Shalom  
 ; Description: this code prints the painting in AsciiArt1. Section A of question 4
 ; ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 .model small
